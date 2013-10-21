fa85d2c05d02685a653a1a26676260cb446a131e
fskreuz <fskreuz@gmail.com>
2013-10-21 12:50:54 +0800

    Optimizations for Has Offers data gathering.

--- client/views/hasoffers/hasoffers.js
+++ client/views/hasoffers/hasoffers.js
@@ -1,7 +1,10 @@
 Template.hasoffers.list = function(){
-	return Games.find({},{sort:{clicks:-1}});
+  return Games.find({},{
+    sort: {name:1}
+    //sort: {clicks:-1,name:-1}
+  });
 }
 
 Template.hasoffers.totals = function(){
-	return Gametotals.find();
+  return Gametotals.find();
 }
--- server/server.js
+++ server/server.js
@@ -1,84 +1,93 @@
-console.log("Server code speaking");
-
-// Wondering where the value for AnyTV.data('hasOffers') comes from?
-// Check out packages/anytv-initializations package
-
-// // Check if it is in the AnyTV storage (or maybe create one)
-// if(AnyTV.data('hasOffers')){
-
-//   // You can immediately fetch and chain
-//   var results = AnyTV.data('hasOffers').Report('getStats',{
-//     fields : ['Stat.clicks','Stat.conversions','Stat.currency','Stat.payout','Offer.name'],
-//     sort : 'Offer.name',
-//     groups : ['Offer.name'],
-//     totals : 1,
-//     limit : 1000000
-//   });
-
-//   // We should get some results
-//   console.log(results);
-
-// }
+console.log('Server code speaking');
 
 
 Meteor.startup(function(){
   
-  Games.remove({});
-  
-  Gametotals.remove({});
-  
-  Videos.remove({});
+  // Has Offers aggregator
+  (function(){
 
-  Meteor.publish("games", function(){
-    return Games.find();
-  });
-  Meteor.publish("gametotals", function(){
-    return Gametotals.find();
-  })
-  Meteor.publish("videos", function(){
-    return Videos.find();
-  })
+    // Clearing the collections on first run
+    Games.remove({});
+    Gametotals.remove({});
 
-  if(AnyTV.data('hasOffers')){
+    // Publish data when clients subscribe
+    Meteor.publish('games', function(){
+      return Games.find();
+    });
+    Meteor.publish('gametotals', function(){
+      return Gametotals.find();
+    });
+
+    // Escape this block when Has Offers doesn't exist
+    if(!AnyTV.data('hasOffers')) return;
+
+    // Storage variables for paged polling
+    var pageCount = -1;
+    var itemCount = -1;
+    var currentPage = 1;
+
+    // Semi-real time data, courtesy of this timer
+    // TODO: Fix scoping of current page variable to fetch pages in order
     Meteor.setInterval(function(){
+      console.log('Fetching Offers Page ',currentPage);
 
+      // TODO: Fix grouping of offer names. Most likely, the cause is the offer_id which can't be grouped`
       var results = AnyTV.data('hasOffers').Report('getStats',{
-        fields : ['Stat.clicks','Stat.conversions','Stat.currency','Stat.payout','Offer.name'],
+        fields : ['Stat.offer_id','Offer.name','Stat.clicks','Stat.conversions','Stat.currency','Stat.payout'],
         sort : 'Offer.name',
         groups : ['Offer.name'],
-        totals : 1,
-        limit : 1000000
+        page : currentPage
       });
 
-      var totalclicks = parseInt(results.data.totals.Stat.clicks);
-      var totalconversions = parseInt(results.data.totals.Stat.conversions);
-      var totalpayout = parseInt(results.data.totals.Stat.payout);
-      var totalcurrency = "USD";
-      if(Gametotals.find().count() === 0){
-        Gametotals.insert({clicks: totalclicks, conversions: totalconversions, payout: totalpayout, currency: totalcurrency});
-      }else{
-        var id = Gametotals.findOne()._id;
-          Gametotals.update({_id:id},{$set:{clicks: totalclicks, conversions: totalconversions, payout: totalpayout, currency: totalcurrency}});
+      // Check if offers data failed, and if so, skip
+      if(results.status === -1) return;
+
+      // Update the counts
+      if(pageCount === -1) pageCount = results.data.pageCount;
+      if(itemCount === -1) itemCount = results.data.count;
+
+      var offer;
+      var offerEntry;
+      var offers = results.data.data;
+      var offerIndex = 0;
+      var offerLength = offers.length;
+
+      // Add to collection the data fetched
+      for(var offerIndex = 0; offerIndex < offerLength; ++offerIndex){
+        offer = offers[offerIndex];
+
+        // Generate the offer entry
+        offerEntry = {
+          offer_id    : offer.Stat.offer_id, 
+          name        : offer.Offer.name,
+          clicks      : parseInt(offer.Stat.clicks), 
+          conversions : parseInt(offer.Stat.conversions), 
+          payout      : parseInt(offer.Stat.payout), 
+          currency    : offer.Stat.currency
+        };
+
+        // Upsert - update existing with given offer_id, or insert
+        Games.upsert(
+          {offer_id : offerEntry.offer_id},
+          offerEntry
+        );
+
       }
 
-      for (var key in results.data.data) {
-        if (results.data.data.hasOwnProperty(key)) {
-          var offer_id = parseInt(results.data.data[key].Offer.id);
-          var clicks = parseInt(results.data.data[key].Stat.clicks);
-          var conversions = parseInt(results.data.data[key].Stat.conversions);
-          var payout = parseInt(results.data.data[key].Stat.payout);
-          var currency = results.data.data[key].Stat.currency;
-          var name = results.data.data[key].Offer.name;
-          if(Games.find({offer_id:offer_id}).count() === 0 ){
-            Games.insert({offer_id: offer_id, clicks: clicks, conversions: conversions, payout: payout, currency: currency, name: name});
-          }else{
-            var id = Games.findOne({offer_id:offer_id})._id;
-            Games.update({_id:id},{$set:{offer_id: offer_id, clicks: clicks, conversions: conversions, payout: payout, currency: currency, name: name}});
-          }
-        }
-      }
-    }, 2000);
-  }
+      // Increment, and when end is reached, reset
+      if(++currentPage > pageCount) currentPage = 1;
+    },2000);
+
+  }());
+
+
+
+
+  
+  Videos.remove({});
+  Meteor.publish('videos', function(){
+    return Videos.find();
+  })
 
 
   if(AnyTV.data('YouTube')){
@@ -98,7 +107,7 @@ Meteor.startup(function(){
       var videos = results.data.items[0].statistics.videoCount;
 
       if(Videos.find({channel_id:channel_id}).count() === 0){
-        console.log("vids in");
+        console.log('vids in');
         Videos.insert({channel_id:channel_id,title:title,views:views,comments:comments,subscribers:subscribers,videos:videos});
       }else{
         var id = Videos.findOne({channel_id:channel_id})._id;
@@ -122,7 +131,7 @@ Meteor.startup(function(){
       var videos = results.data.items[0].statistics.videoCount;
 
       if(Videos.find({channel_id:channel_id}).count() === 0){
-        console.log("vids in");
+        console.log('vids in');
         Videos.insert({channel_id:channel_id,title:title,views:views,comments:comments,subscribers:subscribers,videos:videos});
       }else{
         var id = Videos.findOne({channel_id:channel_id})._id;
@@ -146,7 +155,7 @@ Meteor.startup(function(){
       var videos = results.data.items[0].statistics.videoCount;
 
       if(Videos.find({channel_id:channel_id}).count() === 0){
-        console.log("vids in");
+        console.log('vids in');
         Videos.insert({channel_id:channel_id,title:title,views:views,comments:comments,subscribers:subscribers,videos:videos});
       }else{
         var id = Videos.findOne({channel_id:channel_id})._id;
@@ -170,7 +179,7 @@ Meteor.startup(function(){
       var videos = results.data.items[0].statistics.videoCount;
 
       if(Videos.find({channel_id:channel_id}).count() === 0){
-        console.log("vids in");
+        console.log('vids in');
         Videos.insert({channel_id:channel_id,title:title,views:views,comments:comments,subscribers:subscribers,videos:videos});
       }else{
         var id = Videos.findOne({channel_id:channel_id})._id;
